f9c8b5e72459c1d7bbd3c25ae91c0a1bde886117
Kirill <kirxpe@gmail.com>
2023-11-30 19:45:21 +0700

    test

--- src/form.cpp
+++ src/form.cpp
@@ -20,5 +20,22 @@ void Form::start() {
     QString str = ui->str->text();
     QString sub_str = ui->sub_str->text();
 
-    ui->result->setText("result.....");
+    // Call the method to check if str contains sub_str
+    checkSubstring();
+}
+
+void Form::checkSubstring() {
+    QString str = ui->str->text();
+    QString sub_str = ui->sub_str->text();
+
+    if (containsSubstring(str, sub_str)) {
+        ui->result->setText(str + " contains " + sub_str);
+    } else {
+        ui->result->setText(str + " does not contain " + sub_str);
+    }
+}
+
+bool Form::containsSubstring(const QString &str, const QString &subStr) {
+    // Use contains() method to check if subStr is present in str
+    return str.contains(subStr);
 }
